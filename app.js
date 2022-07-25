588f5051dcc43fcea304c9a5cf341d58f81d723f
Prakhar Vohra <100100922+bitcity999@users.noreply.github.com>
2022-07-26 00:53:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+// =========== Horizontal scroll ===========
+
+function scrollL() {
+    var left = document.querySelector(".scroll-images")
+    left.scrollBy(350, 0)
+}
+
+function scrollR() {
+    var right = document.querySelector(".scroll-images")
+    right.scrollBy(-350, 0)
+}
